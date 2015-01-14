ebb07efe18f228310699da9464d2ad70ba5c924f
ricardodeazambuja <ricardo.azambuja@gmail.com>
2015-01-14 19:22:16 +0000

    Baxter simulated on V-REP writes hello
    Here are the files of a scene in V-REP with a modified version of the
    Baxter model (I deleted the right arm, all the child scripts and made
    everything static, non dynamic, etc). The pen has two separated dummy
    objects and those are used with the IK to position the left arm. The
    pen that is really writing the text is invisible and has the dummy
    objects attached to it (it is possible to use the one Baxter is
    holding, but then if the movement is too fast the IK cannot solve on
    time and the trace becomes not continuum…). The word hello was
    generated moving the Sphere inside the scene “1_writer.ttt” and
    exporting the data generated inside the graph object to a CSV file (it
    is necessary to adjust the buffer configuration of the graph).

---
+++ v-rep_python/hello_writer.py
@@ -0,0 +1,57 @@
+import vrep
+import numpy
+import time
+import sys
+
+import matplotlib.pyplot as plt
+
+hello = numpy.genfromtxt('hello.csv',delimiter=',',skip_header=3,usecols = (1, 2, 3),dtype=numpy.float)
+object_name = 'feltPen_invisible'
+# object_name = 'Sphere'
+
+plt.figure()
+plt.plot(-hello[300:,0],-hello[300:,1])
+plt.show()
+
+
+
+hello = numpy.array(hello[300:,[0,1]])
+
+hello[:,0] = -(hello[:,0]-hello[0,0])/2.0
+hello[:,1] = -(hello[:,1]-hello[0,1])/2.0
+
+
+print 'Program started'
+vrep.simxFinish(-1) # just in case, close all opened connections
+clientID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5)
+if clientID!=-1:
+    print 'Connected to remote API server'
+    res,objs=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_oneshot_wait)
+    if res==vrep.simx_return_ok:
+        print 'Number of objects in the scene: ',len(objs)
+        res,v0=vrep.simxGetObjectHandle(clientID,object_name,vrep.simx_opmode_oneshot_wait)
+        print "Ok, I'm in!"
+        # Reads the pen position X,Y,Z
+        res,pos=vrep.simxGetObjectPosition(clientID,v0,vrep.sim_handle_parent,vrep.simx_opmode_oneshot_wait)
+        print "Initial Position", pos
+        # sys.exit()
+        i = 0
+        for hi in hello:
+            time.sleep(0.05)
+            cmd_pos = numpy.array(pos)+numpy.concatenate([hi,[0]]) # Sums X and Y
+            print "Cmd Position: ", i, hi, pos
+            i+=1
+            # Sets the new position
+            res = vrep.simxSetObjectPosition(clientID,v0,vrep.sim_handle_parent,cmd_pos,vrep.simx_opmode_oneshot_wait)
+            if res!=0:
+                vrep.simxFinish(clientID)
+                print 'Remote API function call returned with error code: ',res
+                break
+
+
+    else:
+        print 'Remote API function call returned with error code: ',res
+    vrep.simxFinish(clientID)
+else:
+    print 'Failed connecting to remote API server'
+print 'Program ended'
